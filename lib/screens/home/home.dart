e394e7ac768772e58877d7a5e63bfa3edb76e4e3
Nawitra <vernandowijayaputra@gmail.com>
2020-10-30 09:45:44 +0700

    home screen UI added

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -11,7 +11,7 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: cream,
       body: Column(
         children: <Widget>[
-          SizedBox(height: 100.0),
+          SizedBox(height: 90.0),
           Container(
             margin: EdgeInsets.all(16.0),
             child: Column(
@@ -20,7 +20,78 @@ class HomeScreen extends StatelessWidget {
               ],
             ),
           ),
-          SizedBox(height: 100.0),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.21),
+          //Info toko
+          Container(
+            width: 256,
+            height: 86,
+            margin: EdgeInsets.only(
+                right: MediaQuery.of(context).size.width * 0.30),
+            child: Column(
+              children: <Widget>[
+                Image.asset('assets/img/info_toko.png'),
+              ],
+            ),
+          ),
+          SizedBox(height: 60.0),
+          //Cari toko
+          Container(
+            width: 256,
+            height: 86,
+            margin: EdgeInsets.only(left: 105),
+            child: Column(
+              children: <Widget>[
+                Image.asset('assets/img/cari_toko.png'),
+              ],
+            ),
+          ),
+          SizedBox(height: 30),
+          Column(
+            children: <Widget>[
+              Text(
+                "Sudah masuk sebagai Yuli Nasution.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 12,
+                  fontFamily: "Roboto",
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+              InkWell(
+                child: Container(
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "Bukan Anda? ",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontFamily: "Roboto",
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                        Text(
+                          "Ganti akun",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Color(0xff6b9080),
+                            fontSize: 12,
+                            decoration: TextDecoration.underline,
+                            fontFamily: "Roboto",
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
